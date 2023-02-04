1ab899621871cdbc1192107187b4322ef2de0dd1
Jatvik0609 <112807026+Jatvik0609@users.noreply.github.com>
2023-02-05 00:02:00 +0530

    Update arrays.java

--- arrays.java
+++ arrays.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 public class arrays {
     public static void main(String[] args) {
         int[] numbers = new int[5];
         numbers[0] = 1;
         numbers[1] = 2;
-        System.out.println(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 }
