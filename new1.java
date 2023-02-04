dbd1b87a3a49257d65df784edfbcab936583c33c
Jatvik0609 <112807026+Jatvik0609@users.noreply.github.com>
2023-02-04 23:49:47 +0530

    java_1

---
+++ new1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class new1 {
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+
+        System.out.println("ENTER THE NUMBER : ");
+
+        int a = s1.nextInt();
+
+        s1.close();
+    }
+}
---
+++ string.java
@@ -0,0 +1,10 @@
+public class string {
+    public static void main(String[] args) {
+        String first_name = "Jatvik";
+        String last_name = "Tarpara";
+
+        System.out.println(first_name + " " + last_name);
+        System.out.println(first_name.length());
+        System.out.println(first_name.charAt(0));
+    }
+}
