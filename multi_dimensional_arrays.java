b2088f4cd39d53318159ddba6239c6fa1e0e16ac
Jatvik0609 <112807026+Jatvik0609@users.noreply.github.com>
2023-02-05 10:28:07 +0530

    Create multi_dimensional_arrays.java

---
+++ multi_dimensional_arrays.java
@@ -0,0 +1,8 @@
+public class multi_dimensional_arrays {
+    public static void main(String[] args) {
+        int[][] numbers = new int[2][3];
+
+        numbers[0][0] = 1;
+        System.out.println();
+    }
+}
